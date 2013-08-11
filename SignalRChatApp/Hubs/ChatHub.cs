7595baed6da0a1ccf20a03b8656fc9c16b5ba99c
Sampath-Lokuge <sugeesampathlokuge@gmail.com>
2013-08-11 16:59:01 +0530

    Chat Application with SignalR

---
+++ SignalRChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the addNewMessageToPage method to update clients
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
